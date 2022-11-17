82854a1e27568071102f50d9f20464746f8ba0a2
dariel0514 <dariel0514@hotmail.com>
2022-11-17 15:24:20 +0000

    Fix bt and add text

--- src/front/js/component/mainpage.js
+++ src/front/js/component/mainpage.js
@@ -14,7 +14,14 @@ export const MainPage = () => {
         <input type="submit" value="Submit" />
       </form>
       <h1>Result:</h1>
-      <div className="result"></div>
+      <div className="result">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </div>
     </div>
   );
 };
