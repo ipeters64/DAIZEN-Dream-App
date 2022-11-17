7b962b529bb16c3f93000538643427fa90ddcb3c
dariel0514 <dariel0514@hotmail.com>
2022-11-17 01:27:18 +0000

    Add new icons

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUser,
+  faPhone,
+  faGear,
+  faCircleInfo,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-box">
-        <li className="navbar-items">Home</li>
-        <li className="navbar-items">Features</li>
-        <li className="navbar-items">Contact</li>
-        <li className="navbar-items">About</li>
-        <li className="navbar-items">User</li>
+        <li className="navbar-items">
+          Home <FontAwesomeIcon icon={faHouse} />
+        </li>
+        <li className="navbar-items">
+          Features <FontAwesomeIcon icon={faGear} />
+        </li>
+        <li className="navbar-items">
+          Contact <FontAwesomeIcon icon={faPhone} />
+        </li>
+        <li className="navbar-items">
+          About <FontAwesomeIcon icon={faCircleInfo} />
+        </li>
+        <li className="navbar-items">
+          User <FontAwesomeIcon icon={faUser} />
+        </li>
       </ul>
     </nav>
   );
