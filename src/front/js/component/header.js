83633621a01d465191528716aee647092b809b5e
ipeters123 <isaiah.peters64@gmail.com>
2022-11-17 01:53:26 +0000

    linked the why do we dream button on homepage

--- src/front/js/component/header.js
+++ src/front/js/component/header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Dreamers from "../../img/Dreamers.jpeg";
-
+import { Link } from "react-router-dom";
 export const Header = () => (
   <div className="header-box">
     <img className="header-logo" src={Dreamers} />
@@ -8,6 +8,8 @@ export const Header = () => (
       <h1 className="header-h1"> DAIZEN Dream App </h1>
       <h2 className="header-h2"> What Your Dream Means</h2>
     </div>
-    <button className="header-btn">Why Do We Dream? </button>
+    <Link to="/demo">
+      <button className="header-btn">Why Do We Dream?</button>{" "}
+    </Link>
   </div>
 );
