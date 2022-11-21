89d28e7fa680e7bc21d973c73001b41d780186c4
dariel0514 <dariel0514@hotmail.com>
2022-11-21 16:15:01 +0000

    Adding

--- src/front/js/component/mainpage.js
+++ src/front/js/component/mainpage.js
@@ -13,15 +13,17 @@ export const MainPage = () => {
         </label>
         <input type="submit" value="Submit" />
       </form>
-      <p className="result">
+      <div className="result">
         <h1>Result:</h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-      </p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+      </div>
     </div>
   );
 };
